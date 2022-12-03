fca9a786dde9cc028ef079b1f8d4c879a3ddedad
girish <giri92321@gmail.com>
2022-12-03 07:26:21 +0000

    adding new file

---
+++ fact123.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "enter the number to find factorial"
+read VAR
+
+NUM=$VAR
+FACT=1
+
+while [ $VAR -gt 1 ]
+do
+	FACT=`expr $FACT \* $VAR`
+	VAR=`expr $VAR - 1`
+done
+echo "factorial of $NUM is $FACT"
